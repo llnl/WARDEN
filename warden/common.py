40daa81dc3a1257b0a567fb9bac01e3ba70ea8ea
BigBadRob0t <vining1@llnl.gov>
2026-09-15 14:39:30 -0500

    Fixed warden_init_or_update command usage on windows

--- warden/common.py
+++ warden/common.py
@@ -249,7 +249,7 @@ def parseFiles(benchmark_config, pickleFilename, dataset_folder, maxNumDays=120,
 
         tmpPickleFilename = Path(Path(pickleFilename).parent/"temp.hkl")
         df.to_pickle(tmpPickleFilename, compression="xz")
-        tmpPickleFilename.rename(pickleFilename)
+        tmpPickleFilename.replace(pickleFilename)
     else:
         logger.info(f"After filtering by date, no files were read in {dataset_folder}. Not writing f{pickleFilename}.")
         pickleFilename.unlink(missing_ok=True)
